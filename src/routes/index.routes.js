cb79b4119524e49eb8398d604ba66560efc7e7ab
mariomora <m.mora.alv@gmail.com>
2023-11-17 15:36:21 -0600

    Se empieza a implementar logger

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -6,15 +6,17 @@ import sessionRouter from "./sessions.routes.js";
 import messageRouter from './messages.routes.js';
 import ticketRouter from './tickets.routes.js';
 import mockingRouter from "./mocking.routes.js";
+import loggertestRouter from "./loggersTest.routes.js";
 
 const router = Router()
 
-router.use('/api/product', productRouter)
-router.use('/api/user', userRouter)
-router.use('/api/carts', cartRouter)
-router.use('/api/sessions', sessionRouter)
+router.use('/api/product', productRouter);
+router.use('/api/user', userRouter);
+router.use('/api/carts', cartRouter);
+router.use('/api/sessions', sessionRouter);
 router.use('/api/messages', messageRouter);
 router.use('/api/tickets', ticketRouter);
-router.use('/mockingproducts', mockingRouter);
+router.use('/api/mockingproducts', mockingRouter);
+router.use('/api/loggerTest', loggertestRouter);
 
 export default router
