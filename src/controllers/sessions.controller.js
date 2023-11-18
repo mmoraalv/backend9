c08b8be60b6632fa07ae969a65417395b676606b
mariomora <m.mora.alv@gmail.com>
2023-11-17 19:57:44 -0600

    Se hacen cambios al controlador de sessions

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,58 +1,56 @@
-import { generateToken } from '../utils/jwt.js';
+import logger from '../utils/logger'; // Importa el logger creado
 
 const postSession = async (req, res) => {
-	try {
-		if (!req.user) {
-			return res.status(401).send({ mensaje: 'Invalidate user' });
-		}
+    try {
+        if (!req.user) {
+            logger.error('Usuario no válido'); // Registro de un error
+            return res.status(401).send({ mensaje: 'Usuario no válido' });
+        }
 
-		// req.session.user = {
-		// 	first_name: req.user.first_name,
-		// 	last_name: req.user.last_name,
-		// 	age: req.user.age,
-		// 	rol: req.user.rol,
-		// 	email: req.user.email,
-		// };
+        const token = generateToken(req.user);
+        res.cookie('jwtCookie', token, {
+            maxAge: 43200000,
+        });
 
-		const token = generateToken(req.user); // se genera el token con el usuario
-		res.cookie('jwtCookie', token, {
-			// se envia el token a las cookies
-			maxAge: 43200000, // seteamos que dure 12 hs en milisegundos
-		});
-
-		return res.redirect('../../static/products');
-	} catch (error) {
-		res.status(500).send({ mensaje: `Error al iniciar sesión ${error}` });
-	}
+        logger.info('Sesión iniciada correctamente'); // Registro de información
+        return res.redirect('../../static/products');
+    } catch (error) {
+        logger.error(`Error al iniciar sesión: ${error}`); // Registro de un error con información adicional
+        res.status(500).send({ mensaje: `Error al iniciar sesión ${error}` });
+    }
 };
 
 const getCurrentSession = async (req, res) => {
-	res.status(200).send(req.user);
+    logger.debug('Obteniendo sesión actual'); // Registro de un mensaje de depuración
+    res.status(200).send(req.user);
 };
 
 const getGithubCreateUser = async (req, res) => {
-	return res.status(200).send({ mensaje: 'Usuario creado' });
+    logger.debug('Usuario de GitHub creado'); // Registro de un mensaje de depuración
+    return res.status(200).send({ mensaje: 'Usuario creado' });
 };
 
 const getGithubSession = async (req, res) => {
-	req.session.user = req.user;
-	res.status(200).send({ mensaje: 'Sesión creada' });
+    logger.debug('Sesión de GitHub creada'); // Registro de un mensaje de depuración
+    req.session.user = req.user;
+    res.status(200).send({ mensaje: 'Sesión creada' });
 };
 
 const getLogout = (req, res) => {
-	if (req.session) {
-		req.session.destroy();
-	}
-	res.clearCookie('jwtCookie');
-	res.status(200).send({ resultado: 'Login eliminado', message: 'Logout' });
+    if (req.session) {
+        req.session.destroy();
+    }
+    res.clearCookie('jwtCookie');
+    logger.info('Sesión cerrada'); // Registro de información sobre cierre de sesión
+    res.status(200).send({ resultado: 'Login eliminado', message: 'Logout' });
 };
 
 const sessionController = {
-	postSession,
-	getCurrentSession,
-	getGithubCreateUser,
-	getGithubSession,
-	getLogout,
+    postSession,
+    getCurrentSession,
+    getGithubCreateUser,
+    getGithubSession,
+    getLogout,
 };
 
 export default sessionController;
